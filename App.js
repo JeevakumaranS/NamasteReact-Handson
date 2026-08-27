6c3483b1f5104d3a00d0aa9cce8428ee534c7e7c
JeevakumaranS <jeevas0028@gmail.com>
2026-08-27 15:36:27 +0530

    Chapter-3 Laying the Foundation

--- App.js
+++ App.js
@@ -2,9 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 
-const heading = React.createElement("h1",{},"Hello");
-
-const heading2 = React.createElement("h1",{},"Hello2");
+const heading = React.createElement("h1",{key:"1"},"Hello");
+console.log(heading);
+const heading2 = React.createElement("h1",{key:"2"},"Hello2");
 
 const div = React.createElement("div",{id:"container"},[heading,heading2]);
 
